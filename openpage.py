8e49afe0f50f53d122b4da0157959ef14d76c3c3
qingxuantang <xiucatwithmark@gmail.com>
2022-07-11 10:15:03 +0800

    add new symbol tradingview page.

--- openpage.py
+++ openpage.py
@@ -2,13 +2,15 @@
 def openWebPage():
     import os
     url =[
-       'https://www.tradingview.com/chart/caDek3Z6/',
-       'https://www.tradingview.com/chart/O5w6cxmD/',
-       'https://www.tradingview.com/chart/VpgCvDG8/',
-       #'https://www.tradingview.com/chart/RBVJp2WS/', 铭普光磁
-       'https://www.tradingview.com/chart/g5CelBO9/',
-       'https://www.tradingview.com/chart/fBmBFdIX/',
-       'https://www.tradingview.com/chart/1qYvHMZe/',]
+        'https://www.tradingview.com/chart/1qYvHMZe/', #BTCUSD
+        'https://www.tradingview.com/chart/caDek3Z6/', #鼎盛新材
+        'https://www.tradingview.com/chart/O5w6cxmD/', #福莱特
+        'https://www.tradingview.com/chart/VpgCvDG8/', #海源复材
+        #'https://www.tradingview.com/chart/RBVJp2WS/', #铭普光磁
+        'https://www.tradingview.com/chart/g5CelBO9/', #明德生物
+        'https://www.tradingview.com/chart/fBmBFdIX/', #中航光电
+        'https://www.tradingview.com/chart/Mr0LbiKB/', #金智科技
+       ]
     for i in url:
         os.system("python -m webbrowser " + i)
 
