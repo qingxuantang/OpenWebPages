3cc70b742cbb8f41d58de888b88a599ea020174c
qingxuantang <xiucatwithmark@gmail.com>
2022-11-08 10:01:24 +0800

    Add now symbol.

--- openpage.py
+++ openpage.py
@@ -6,6 +6,7 @@ def openWebPage():
         'https://www.tradingview.com/chart/caDek3Z6/', #鼎盛新材
         'https://www.tradingview.com/chart/O5w6cxmD/', #福莱特
         'https://www.tradingview.com/chart/g5CelBO9/', #明德生物
+        'https://www.tradingview.com/chart/Mr0LbiKB/', #南玻A
         'https://www.tradingview.com/chart/fBmBFdIX/', #科伦药业, SR > 5
         'https://www.tradingview.com/chart/VpgCvDG8/', #巨星科技, SR > 2
         
