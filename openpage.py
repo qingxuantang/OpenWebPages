aa4b27da2718d8b6ccd4c43570cba85e0df1d1a8
qingxuantang <xiucatwithmark@gmail.com>
2023-02-08 00:24:21 +0800

    Update symbol pool.

--- openpage.py
+++ openpage.py
@@ -2,14 +2,23 @@
 import os
 url =[
     'https://www.tradingview.com/chart/1qYvHMZe/', #BTCUSD
+    'https://www.tradingview.com/chart/WFQjAmDL/', #小米
     'https://www.tradingview.com/chart/caDek3Z6/', #鼎盛新材
     'https://www.tradingview.com/chart/O5w6cxmD/', #福莱特
     'https://www.tradingview.com/chart/g5CelBO9/', #明德生物
-    'https://www.tradingview.com/chart/fBmBFdIX/', #科伦药业, SR > 5
-    'https://www.tradingview.com/chart/VpgCvDG8/', #巨星科技, SR > 2
-    'https://www.tradingview.com/chart/RBVJp2WS/', #荣盛发展，SR > 5
+    'https://www.tradingview.com/chart/fBmBFdIX/', #科伦药业, SR = 8
+    'https://www.tradingview.com/chart/VpgCvDG8/', #巨星科技, SR = 4
+    'https://www.tradingview.com/chart/RBVJp2WS/', #中钨高新
+    'https://www.tradingview.com/chart/uIOtTO4I/', #深深房A
+    'https://www.tradingview.com/chart/Mr0LbiKB/', #南玻A
+    'https://www.tradingview.com/chart/24PY4ZGk/', #芯能科技
+    'https://www.tradingview.com/chart/xN3apmRm/', #和而泰
+    'https://www.tradingview.com/chart/dEaD5qXm/', #万邦达, SR = 6.8
+    'https://www.tradingview.com/chart/E65JE3rd/', #GQY视讯, SR = 3.36
+    'https://www.tradingview.com/chart/j8Iu5MEr/', #奥飞数据, SR = 2.89
+    
+    
 
-    #'https://www.tradingview.com/chart/Mr0LbiKB/', #南玻A
     #'https://www.tradingview.com/chart/VpgCvDG8/', #古井贡酒
     #'https://www.tradingview.com/chart/Mr0LbiKB/', #招商银行
     #'https://www.tradingview.com/chart/VpgCvDG8/', #海源复材
